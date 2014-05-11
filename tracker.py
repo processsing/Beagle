d16595ce548992ad62c1a3328942fcda8a2f483c
processsing <jabran.pro@gmail.com>
2014-05-11 14:24:36 -0400

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,10 +1,5 @@
 ###############################################################################
-# Universal Analytics for Python
-# Copyright (c) 2013, Analytics Pros
-# 
-# This project is free software, distributed under the BSD license. 
-# Analytics Pros offers consulting and integration services if your firm needs 
-# assistance in strategy, implementation, or auditing existing work.
+Use this to capture data
 ###############################################################################
 
 from urllib2 import urlopen, build_opener, install_opener
